aae569cb0dde1e08340232079fb3a6fbc77198b0
Guilherme Afonso <guilherme-afonso@outlook.com>
2022-03-04 14:17:43 -0300

    Update main-menu.php

--- assets/includes/main-menu.php
+++ assets/includes/main-menu.php
@@ -1,10 +1,10 @@
 <div id="main-menu">
     <a href="./">Início</a>
     <a href="./guilda">A Guilda</a>
-    <a href="./eventos">Eventos</a>
-    <a href="./regras">Nossas regras</a>
-    <a href="./discord">Discord</a>
-    <a href="./contato">Contato</a>
+    <a href="#">Eventos</a>
+    <a href="#">Nossas regras</a>
+    <a href="#">Discord</a>
+    <a href="#">Contato</a>
 
     <!-- <div class="separador"></div>
     <a href="./login"><i class="ri-user-line"></i> Login</a> -->
