a08751fac6cbb23a280a08347fc05740611f0138
Guilherme Afonso <guilherme-afonso@outlook.com>
2022-03-04 14:25:47 -0300

    Update scripts.php

--- assets/includes/scripts.php
+++ assets/includes/scripts.php
@@ -79,24 +79,4 @@ function strip_php_extension()
         redirect($url);
     }
 }
-
-//==== Redirect. Try PHP header redirect, then Java, then http redirect
-function redirect($url)
-{
-    if (!headers_sent()) {
-        /* If headers not yet sent => do php redirect */
-        header('Location: ' . $url);
-        exit;
-    } else {
-        /* If headers already sent => do javaScript redirect */
-        echo '<script type="text/javascript">';
-        echo 'window.location.href="' . $url . '";';
-        echo '</script>';
-
-        /* If javaScript is disabled => do html redirect */
-        echo '<noscript>';
-        echo '<meta http-equiv="refresh" content="0; url=' . $url . '" />';
-        echo '</noscript>';
-        exit;
-    }
-}
+?>
