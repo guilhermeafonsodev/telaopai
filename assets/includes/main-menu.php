ddad924c0c04149c3831e6546deff280e7b14296
Guilherme Afonso <guilherme-afonso@outlook.com>
2022-03-04 14:27:22 -0300

    Update main-menu.php

--- assets/includes/main-menu.php
+++ assets/includes/main-menu.php
@@ -1,6 +1,6 @@
 <div id="main-menu">
     <a href="./">Início</a>
-    <a href="./guilda">A Guilda</a>
+    <a href="./guilda.php">A Guilda</a>
     <a href="#">Eventos</a>
     <a href="#">Nossas regras</a>
     <a href="#">Discord</a>
@@ -8,7 +8,7 @@
 
     <!-- <div class="separador"></div>
     <a href="./login"><i class="ri-user-line"></i> Login</a> -->
-    <button onclick="location.href='./inscrever'" type="button" class="inscrever">
+    <button onclick="location.href='./inscrever.php'" type="button" class="inscrever">
         <p><i class="ri-user-add-line"></i> Seja membro</p>
     </button>
 </div>
